ca44d71c08d128bdcc60d7885c531e3b9495624e
ylk01 <fvd130@aubg.edu>
2016-10-22 02:49:49 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+var assert = require('assert'); 
+var maybeFirst = require('../server.js');
+
+describe('maybeFirst', function() {
+  it('returns the first element of an array', function() {
+    var result = "";
+
+    assert.equal(result, "", 'expected empty'); //expected empty array when start
+  });
+});
